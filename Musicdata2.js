6439a3cf4dc5db4cf37211f75f5269c5f02db4d3
Katuord <0821027ko@shikokuchuo.ed.jp>
2026-09-10 10:49:57 +0900

    Create Musicdata2.js

---
+++ Musicdata2.js
@@ -0,0 +1,26 @@
+// Musicdata2.js
+window.songList2 = [
+    {
+        id: "music2",
+        title: "Cration of Hatred (slowed)",
+        artist: "in Forsaken",
+        scUrl: "https://soundcloud.com/alexandru-r-doi-326628379/roblox-forsaken-creation-of",
+        levels: { easy: "Easy Lv.4", normal: "Normal Lv.5", hard: "Hard Lv.11" },
+        charts: {
+            easy: [
+                {time: 2.0, lane: 0}, {time: 2.5, lane: 1}, {time: 3.0, lane: 2}, {time: 3.5, lane: 3},
+                {time: 4.5, lane: 0, isLong: true, endTime: 5.5}, {time: 6.0, lane: 1}, {time: 6.5, lane: 2},
+                {time: 7.0, lane: 3}, {time: 8.0, lane: 1, isLong: true, endTime: 9.5}, {time: 10.0, lane: 0}
+            ],
+            normal: [
+                {"time: 1.0, lane: 0}
+            ],
+            hard: [
+                {time: 1.0, lane: 0}, {time: 1.2, lane: 1}, {time: 1.4, lane: 2}, {time: 1.6, lane: 3},
+                {time: 2.0, lane: 2}, {time: 2.2, lane: 1}, {time: 2.4, lane: 0}, {time: 2.6, lane: 3},
+                {time: 3.0, lane: 0, isLong: true, endTime: 4.5}, {time: 3.5, lane: 3, isLong: true, endTime: 5.0},
+                {time: 5.5, lane: 1}, {time: 5.7, lane: 2}, {time: 5.9, lane: 1}, {time: 6.1, lane: 2}
+            ]
+        }
+    },
+];
