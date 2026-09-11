62a832dfe07c4b6a7cca44c0857965278a6d3423
Katuord <0821027ko@shikokuchuo.ed.jp>
2026-09-11 11:18:01 +0900

    Update Musicdata3.js

--- Musicdata3.js
+++ Musicdata3.js
@@ -1,1 +1,24 @@
-https://soundcloud.com/tg-claps-gabe-m/key-after-key-plead-slowed
+
+// Musicdata2.js
+window.songList2 = [
+    {
+        id: "music2",
+        title: "PLEAD (slowed)",
+        artist: "in Forsaken",
+        scUrl: "https://soundcloud.com/tg-claps-gabe-m/key-after-key-plead-slowed",
+        levels: { easy: "Easy Lv.4", normal: "Normal Lv.7", hard: "Hard Lv.9" },
+        charts: {
+            easy: [
+                {time: 2.0, lane: 0}, {time: 2.5, lane: 1}, {time: 3.0, lane: 2}, {time: 3.5, lane: 3},
+                {time: 4.5, lane: 0, isLong: true, endTime: 5.5}, {time: 6.0, lane: 1}, {time: 6.5, lane: 2},
+                {time: 7.0, lane: 3}, {time: 8.0, lane: 1, isLong: true, endTime: 9.5}, {time: 10.0, lane: 0}
+            ],
+            normal: [
+                {time: 1.0, lane: 0}
+            ],
+            hard: [
+              {time:1.0, lane: 0}
+            ]
+        }
+    },
+];
