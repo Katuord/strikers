fe11c40ff255570c3803ab2fe010552d93c4ab4e
Katuord <0821027ko@shikokuchuo.ed.jp>
2026-09-11 13:14:18 +0900

    Update Musicdata4.js

--- Musicdata4.js
+++ Musicdata4.js
@@ -1,1 +1,27 @@
-https://soundcloud.com/user-930972064/sols-rng-pixelation-check-des?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing
+
+// Musicdata4.js
+window.songList4 = [
+    {
+        id: "music3",
+        title: "CENSORED!! [PIXELATION THEME]",
+        artist: "in Sol's ",
+        scUrl: "https://soundcloud.com/ryan-nguyen-883211384/tpazolite-censored-full-ver",
+        levels: { easy: "Easy Lv.7", normal: "Normal Lv.7", hard: "Hard Lv.7", expert: "Expert Lv.7 },
+        charts: {
+            easy: [
+                {time: 2.0, lane: 0}, {time: 2.5, lane: 1}, {time: 3.0, lane: 2}, {time: 3.5, lane: 3},
+                {time: 4.5, lane: 0, isLong: true, endTime: 5.5}, {time: 6.0, lane: 1}, {time: 6.5, lane: 2},
+                {time: 7.0, lane: 3}, {time: 8.0, lane: 1, isLong: true, endTime: 9.5}, {time: 10.0, lane: 0}
+            ],
+            normal: [
+                {time: 1.0, lane: 0}
+            ],
+            hard: [
+              {time:1.0, lane: 0}
+            ],
+            expert: [
+               {time:1.0,lane:0}
+            ]
+        }
+    },
+];
