5d0c3e811fccfcc0af828e26f82b9104458c6168
Katuord <0821027ko@shikokuchuo.ed.jp>
2026-09-14 13:14:16 +0900

    Create Musicdata5.js

---
+++ Musicdata5.js
@@ -0,0 +1,25 @@
+
+// Musicdata5.js
+window.songList5 = [
+    {
+        id: "music5",
+        title: "ゲンチアナ [slowed]",
+        artist: "nil",
+        scUrl: "https://soundcloud.com/iuvolauzrsr5/gentiana-slowed-reverb?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        levels: { easy: "Easy Lv.7", normal: "Normal Lv.10", hard: "Hard Lv.19", expert: "Expert Lv.22" },
+        charts: {
+            easy: [
+                {time: 2.0, lane: 0}, {time: 2.5, lane: 1}, {time: 3.0, lane: 2}, {time: 3.5, lane: 3},
+                {time: 4.5, lane: 0, isLong: true, endTime: 5.5}, {time: 6.0, lane: 1}, {time: 6.5, lane: 2},
+                {time: 7.0, lane: 3}, {time: 8.0, lane: 1, isLong: true, endTime: 9.5}, {time: 10.0, lane: 0}
+            ],
+            normal: [
+                {time: 1.0, lane: 0}
+            ],
+            hard: [
+              {time:1.0, lane: 0}
+            ],
+            expert: [{time:1.0, lane: 0}]
+      }
+    },
+];
