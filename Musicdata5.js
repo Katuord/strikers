f6b71146f341a8c0efc5d7c86c106821aa135b91
Katuord <0821027ko@shikokuchuo.ed.jp>
2026-09-17 13:14:24 +0900

    Update Musicdata5.js

--- Musicdata5.js
+++ Musicdata5.js
@@ -3,9 +3,9 @@
 window.songList5 = [
     {
         id: "music5",
-        title: "ゲンチアナ [slowed]",
+        title: "抜錨",
         artist: "nil",
-        scUrl: "https://soundcloud.com/iuvolauzrsr5/gentiana-slowed-reverb?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        scUrl: "https://soundcloud.com/rvrzbckglcte/3j6lgjjtulwi?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
         levels: { easy: "Easy Lv.7", normal: "Normal Lv.10", hard: "Hard Lv.19", expert: "Expert Lv.22" },
         charts: {
             easy: [
